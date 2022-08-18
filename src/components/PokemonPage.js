a241748d1f7526f08690886d62c3c425f6d7683f
Henry Escobar <henrye@gmail.com>
2022-08-18 13:47:43 -0700

    commit tree c991e4c7a41ccb6463f0edf5fc6c597a48bfd923

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+      .then((r) => r.json())
+      .then((pokemons) => {
+        setPokemons(pokemons);
+      });
+  }, []);
+
+  const PokemonDisplay = pokemons.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={PokemonDisplay} />
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Search() {
+function Search({ searchTerm, onSearch }) {
+  function handleSearch(e) {
+    onSearch(e.target.value);
+  }
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input value={searchTerm} onChange={handleSearch} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
